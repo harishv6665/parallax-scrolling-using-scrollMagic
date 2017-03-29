a6b78bb076da87583fbe9d214b1b4c84b7693b79
harish-rzt <harish@Harish-V.local>
2017-03-29 18:56:22 +0530

    Made fully configurable (x, y & rotation)

--- scripts/main.js
+++ scripts/main.js
@@ -2,24 +2,75 @@ $(document).ready(function () {
 
     let scrollMagicController = new ScrollMagic.Controller();
 
-    let sceneElements = document.querySelectorAll("[data-secne]");
+    let sceneElements = document.querySelectorAll("[parallax-scene]");
 
-    let getTweenValue = (depth) => {
+    let getTweenValue = (speed, reverse) => {
         const overallSpeedController = 20;
-        return  (window.innerHeight / 100) * (depth * overallSpeedController);
+        let tweenVal = (window.innerHeight / 100) * (speed * overallSpeedController);
+        if(reverse) {
+            return -1 * tweenVal
+        } else {
+            return tweenVal;
+        }
+    }
+
+    let isDecendent = (parentElements, childElement) => (
+        (parentElements.length > 0) && (parentElements[0].contains(childElement))
+    )
+
+    let getTweenAnimation = (config, reverse) => {
+
+        let tween = "";
+        let willChange = '"willChange": "transform", ';
+        let ease = '"ease": "Linear.easeNone"';
+
+        for (var key in config) {
+            switch(key) {
+                case 'speedX':
+                    tween += `"x": ${getTweenValue(config[key], reverse)}, `;
+                    break;
+                case 'speedY':
+                    tween += `"y": ${getTweenValue(config[key], reverse)}, `
+                    break;
+                case 'rotation':
+                    tween += `"rotation": ${getTweenValue(config[key], reverse)}, `
+                    break;
+                default:
+                    return tween;
+            }
+        }
+
+        tween += willChange;
+        tween += ease;
+        tween = '{' + tween + '}'
+        console.log("tween: ", JSON.parse(tween));
+
+        return JSON.parse(tween);
+
     }
 
     let getTween = (element) => {
         let tween = new TimelineMax();
-        let tweenElements = element.querySelectorAll("[data-depth]");
+        let tweenElements = element.querySelectorAll("[parallax-element]");
+
+        let nestedSceneElements = element.querySelectorAll("[parallax-scene]");
+
         for (let item = 0; item <= tweenElements.length - 1; item++) {
-            tween.fromTo(
-                tweenElements[item],
-                1,
-                {y: getTweenValue(tweenElements[item].getAttribute('data-depth')) + 'px', willChange: 'transform', ease: Linear.easeNone},
-                {y: -1 * getTweenValue(tweenElements[item].getAttribute('data-depth')) + 'px', willChange: 'transform', ease: Linear.easeNone},
-                0
-            )
+
+            if(isDecendent(nestedSceneElements, tweenElements[item])) { break; }
+
+            let config = JSON.parse(tweenElements[item].getAttribute("parallax-element"));
+
+            let fromTween = getTweenAnimation(config, false);
+            let toTween = getTweenAnimation(config, true);
+
+                tween.fromTo(
+                    tweenElements[item],
+                    1,
+                    fromTween,
+                    toTween,
+                    0
+                )
         }
         return tween;
     }
